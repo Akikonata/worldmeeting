5818dc27dcba2a27a0fa9797c74332659d8d4d05
Akikonata <66875692@qq.com>
2014-08-21 14:20:12 +0800

    added animate

--- js/play.js
+++ js/play.js
@@ -1,19 +1,49 @@
 (function() {
-	var shooter_positions = [{
-
+	var shooterPositions = [{
+		right: 150,
+		top: 300
+	}, {
+		right: 190,
+		top: 300
+	}, {
+		right: 290,
+		top: 500
+	}, {
+		right: 350,
+		top: 700
+	}, {
+		right: 150,
+		top: 500
+	}, {
+		right: 250,
+		top: 700
 	}];
 	//循环动画
 	var chinamap = $("#chinamap");
 	var worldmap = $("#worldmap");
 	var china = $("#china");
 	var shooter = $("#shooter");
+	var floatlayer = $("#float-layer");
+	//中国地图放大
 	chinamap.delay(5000).animate({
 		width: 1000,
 		height: 840,
 		right: 268,
 		top: 137
 	}, 2000, function() {
+		//靶位移动
+		shooter.css(shooterPositions[0]);
+		var posIdx = 0;
 		shooter.delay(2000).show();
+		var showCities = function() {
+			posIdx++;
+			if (posIdx >= shooterPositions.length) {
+				return false;
+			}
+			shooter.animate(shooterPositions[posIdx], 1000);
+			setTimeout(showCities, 20000);
+		};
+		setTimeout(showCities, 5000);
 	});
 	worldmap.delay(5000).fadeOut(2000);
 
