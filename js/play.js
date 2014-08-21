c6a50669843a19e82a9b07b872f80062f33200a9
Akikonata <66875692@qq.com>
2014-08-21 12:04:38 +0800

    added animate

--- js/play.js
+++ js/play.js
@@ -1,6 +1,14 @@
 (function() {
 	//循环动画
-
+	var chinamap = $("#chinamap");
+	var worldmap = $("#worldmap");
+	chinamap.delay(5000).animate({
+		width: 1000,
+		height: 840,
+		right: 268,
+		top: 137
+	}, 2000);
+	worldmap.delay(5000).fadeOut(2000);
 	var loop = function() {};
 	loop();
 })();
