4cbfd859e14d0acbcbed82ec03e2c9b77654e19e
Akikonata <66875692@qq.com>
2014-08-21 15:40:15 +0800

    added animate

--- js/play.js
+++ js/play.js
@@ -30,6 +30,7 @@
 	var china = $("#china");
 	var shooter = $("#shooter");
 	var floatlayer = $("#float-layer");
+	var citytext = $("#city-text");
 	//中国地图放大
 	chinamap.delay(5000).animate({
 		width: 1000,
@@ -38,8 +39,11 @@
 		top: 137
 	}, 2000, function() {
 		//靶位移动
-		shooter.css(shooterPositions[0]);
-		var posIdx = 0;
+		shooter.css({
+			right: 268,
+			top: 137
+		});
+		var posIdx = -1;
 		shooter.delay(2000).show();
 		var showCities = function() {
 			posIdx++;
@@ -47,6 +51,7 @@
 				return false;
 			}
 			shooter.animate(shooterPositions[posIdx], 1000, function() {
+				citytext.text(shooterPositions[posIdx].name);
 				floatlayer.delay(1000).animate({
 					width: 1658,
 					height: 722
@@ -55,7 +60,7 @@
 					height: 0
 				}, 1000);
 			});
-			setTimeout(showCities, 20000);
+			setTimeout(showCities, 15000);
 		};
 		setTimeout(showCities, 5000);
 	});
