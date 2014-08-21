e45d918a52a93668e6e1296e6493409f6eefc180
Akikonata <66875692@qq.com>
2014-08-21 16:17:40 +0800

    added carousel

--- js/play.js
+++ js/play.js
@@ -1,4 +1,10 @@
 (function() {
+	var chinamap = $("#chinamap");
+	var worldmap = $("#worldmap");
+	var china = $("#china");
+	var shooter = $("#shooter");
+	var floatlayer = $("#float-layer");
+	var citytext = $("#city-text");
 	var shooterPositions = [{
 		name: "北京",
 		right: 150,
@@ -24,47 +30,95 @@
 		right: 250,
 		top: 700
 	}];
-	//循环动画
-	var chinamap = $("#chinamap");
-	var worldmap = $("#worldmap");
-	var china = $("#china");
-	var shooter = $("#shooter");
-	var floatlayer = $("#float-layer");
-	var citytext = $("#city-text");
-	//中国地图放大
-	chinamap.delay(5000).animate({
-		width: 1000,
-		height: 840,
-		right: 268,
-		top: 137
-	}, 2000, function() {
-		//靶位移动
-		shooter.css({
+	var reset = function() {
+		worldmap.find("path").attr("class", "");
+		chinamap.find("path").attr("class", "");
+		worldmap.fadeIn(0);
+		play();
+	};
+	var play = function() {
+		//开场动画
+		worldmap.find("path").attr("class", "render");
+		chinamap.find("path").attr("class", "render");
+		//中国地图变大
+		chinamap.delay(5000).animate({
+			width: 1000,
+			height: 840,
 			right: 268,
 			top: 137
-		});
-		var posIdx = -1;
-		shooter.delay(2000).show();
-		var showCities = function() {
-			posIdx++;
-			if (posIdx >= shooterPositions.length) {
-				return false;
-			}
-			shooter.animate(shooterPositions[posIdx], 1000, function() {
-				citytext.text(shooterPositions[posIdx].name);
-				floatlayer.delay(1000).animate({
-					width: 1658,
-					height: 722
-				}, 1000).delay(10000).animate({
-					width: 0,
-					height: 0
-				}, 1000);
+		}, 2000, function() {
+			//靶位移动
+			shooter.css({
+				right: 268,
+				top: 137
 			});
-			setTimeout(showCities, 15000);
-		};
-		setTimeout(showCities, 5000);
-	});
-	worldmap.delay(5000).fadeOut(2000);
-	// var loop = function() {};
-	// loop();
+			var posIdx = -1;
+			shooter.delay(2000).show();
+			var showCities = function() {
+				posIdx++;
+				if (posIdx >= shooterPositions.length) {
+					//中国地图变小
+					chinamap.delay(5000).animate({
+						width: 200,
+						height: 240,
+						right: 362,
+						top: 420
+					}, 2000, function() {
+						reset();
+					});
+					return false;
+				}
+				shooter.animate(shooterPositions[posIdx], 1000, function() {
+					citytext.text(shooterPositions[posIdx].name);
+					floatlayer.delay(1000).animate({
+						width: 1658,
+						height: 722
+					}, 1000).delay(10000).animate({
+						width: 0,
+						height: 0
+					}, 1000);
+				});
+				setTimeout(showCities, 15000);
+			};
+			setTimeout(showCities, 5000);
+		});
+		//世界地图同时fadeOut
+		worldmap.delay(5000).fadeOut(2000);
+	};
+	play();
+	//循环动画
+	//中国地图放大
+	// chinamap.delay(5000).animate({
+	// 	width: 1000,
+	// 	height: 840,
+	// 	right: 268,
+	// 	top: 137
+	// }, 2000, function() {
+	// 	//靶位移动
+	// 	shooter.css({
+	// 		right: 268,
+	// 		top: 137
+	// 	});
+	// 	var posIdx = -1;
+	// 	shooter.delay(2000).show();
+	// 	var showCities = function() {
+	// 		posIdx++;
+	// 		if (posIdx >= shooterPositions.length) {
+	// 			return false;
+	// 		}
+	// 		shooter.animate(shooterPositions[posIdx], 1000, function() {
+	// 			citytext.text(shooterPositions[posIdx].name);
+	// 			floatlayer.delay(1000).animate({
+	// 				width: 1658,
+	// 				height: 722
+	// 			}, 1000).delay(10000).animate({
+	// 				width: 0,
+	// 				height: 0
+	// 			}, 1000);
+	// 		});
+	// 		setTimeout(showCities, 15000);
+	// 	};
+	// 	setTimeout(showCities, 5000);
+	// });
+	// worldmap.delay(5000).fadeOut(2000);
 })();
