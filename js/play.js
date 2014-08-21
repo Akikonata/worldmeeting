410497b2569d84b3e7866e8d004d3e56783d47f4
Akikonata <66875692@qq.com>
2014-08-21 16:35:35 +0800

    added carousel

--- js/play.js
+++ js/play.js
@@ -86,39 +86,4 @@
 		worldmap.delay(5000).fadeOut(2000);
 	};
 	play();
-	//循环动画
-	//中国地图放大
-	// chinamap.delay(5000).animate({
-	// 	width: 1000,
-	// 	height: 840,
-	// 	right: 268,
-	// 	top: 137
-	// }, 2000, function() {
-	// 	//靶位移动
-	// 	shooter.css({
-	// 		right: 268,
-	// 		top: 137
-	// 	});
-	// 	var posIdx = -1;
-	// 	shooter.delay(2000).show();
-	// 	var showCities = function() {
-	// 		posIdx++;
-	// 		if (posIdx >= shooterPositions.length) {
-	// 			return false;
-	// 		}
-	// 		shooter.animate(shooterPositions[posIdx], 1000, function() {
-	// 			citytext.text(shooterPositions[posIdx].name);
-	// 			floatlayer.delay(1000).animate({
-	// 				width: 1658,
-	// 				height: 722
-	// 			}, 1000).delay(10000).animate({
-	// 				width: 0,
-	// 				height: 0
-	// 			}, 1000);
-	// 		});
-	// 		setTimeout(showCities, 15000);
-	// 	};
-	// 	setTimeout(showCities, 5000);
-	// });
-	// worldmap.delay(5000).fadeOut(2000);
 })();
