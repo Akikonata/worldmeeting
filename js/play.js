308917f071363e361e92e49bfe5b8a03962efa69
Akikonata <66875692@qq.com>
2014-08-21 12:46:58 +0800

    dev

--- js/play.js
+++ js/play.js
@@ -1,14 +1,22 @@
 (function() {
+	var shooter_positions = [{
+
+	}];
 	//循环动画
 	var chinamap = $("#chinamap");
 	var worldmap = $("#worldmap");
+	var china = $("#china");
+	var shooter = $("#shooter");
 	chinamap.delay(5000).animate({
 		width: 1000,
 		height: 840,
 		right: 268,
 		top: 137
-	}, 2000);
+	}, 2000, function() {
+		shooter.delay(2000).show();
+	});
 	worldmap.delay(5000).fadeOut(2000);
-	var loop = function() {};
-	loop();
+
+	// var loop = function() {};
+	// loop();
 })();
