1b357e748bb293ef58ee52a8f4c47c4896ed46f4
Akikonata <66875692@qq.com>
2014-08-26 15:53:23 +0800

    added data

---
+++ js/libs/column.js
@@ -0,0 +1,160 @@
+(function(){
+
+    function setColumnConfig( xArr, yArr, duration ){
+        var columnConfig = {
+            "color" : [ '#1782fb'],
+
+            "xAxis": {
+                "categories": xArr,
+
+                "ticks" : {
+                    "enabled" : true,
+                    color : '#3d4659',
+                    dash : [1],
+                    width : 1
+                },
+                "margin" : {
+                    "right" : 10,
+                    "left" : 60
+                },
+
+                "axis" : {
+                    enabled : true,
+                    arrow : false,
+                    color : '#8591a1'
+                },
+                "padding" : {
+                    "left" : 20,
+                    "right" : 20
+                },
+                "label" : {
+                    "enabled" : true,
+                    "font" : {
+                        "color" : "#FFF",
+                        fontSize : 12
+                    }
+                },
+                "min" : 0
+            },
+
+            "yAxis": {
+                "min" : 0,
+                "ticks" : {
+                    "enabled" : true,
+                    color : '#3d4659',
+                    dash : [1],
+                    width : 1
+                },
+                // grid : [-0.3, 0, 0.3, 0.6, 0.9],
+                "label" : {
+                    "enabled" : true,
+                    "rotate" : 0,
+                    "font" : {
+                        "color" : "#FFF",
+                        fontSize : 12
+                    },
+                    format : function(num) {
+                        return num;
+                    }
+                },
+                unit : {
+                    text : '消息数',
+                    "font" : {
+                        "color" : "#9096a1",
+                        fontSize : 12
+                    }
+                },
+                "axis" : {
+                    enabled : true,
+                    arrow : false
+                },
+                "padding" : {
+                    "top" : 0,
+                    "bottom" : 0
+                }
+            },
+
+            "plotOptions" : {
+
+                "column" : {
+                    "width" : 14,
+                    "margin": 1
+                },
+
+                "label" : {
+                    "enabled" : false
+                }
+
+            },
+
+            "legend" : {
+                "enabled" : false
+            },
+
+            "interaction" : {
+                "onStickHover" : null
+            },
+            animation:{
+                enabled : true,
+                duration: duration || 1000,
+                mode : 'ease',
+                delayInterval : 200
+            },
+            "series": [
+                {
+                    "name": "北京即时推送消息数",
+                    "data": yArr
+                }
+            ]
+
+        };
+
+        return columnConfig;
+    }
+    
+    window.Column = {
+    	init : function( id ){
+    		this.container = $('#' + id)[0];
+    		this.chart = this.chart || new kc.ColumnChart(id);
+    	},
+
+    	update : function( data, duration ){
+    		this.data = data;
+    		this.animDur = duration || 1000;
+
+    		this.conf = setColumnConfig( data.x, data.y, this.animDur );
+    		this.chart.update( this.conf );
+
+            var self = this;
+            setTimeout(function(){
+                self.colorMax();
+            }, 1000);
+            
+    	},
+
+        colorMax : function(){
+            var arr = this.data.y,
+                l = arr.length,
+                max = 0,
+                t = 0;
+
+            for(var i = 0; i < l; i++){
+                if( arr[i] > max ){
+                    max = arr[i];
+                    t = i;
+                }
+            }
+
+            var sticks = this.chart.getPlots().getPlotsElements().getElementList();
+            sticks[ l-t-1 ].update({
+                color : '#f0294a'
+            });
+        },
+
+        setTotal : function(num){
+            num += '';
+            $('.column .number').html( num.replace(/(?=(?!\b)(?:\d{3})+(?!\d))/g, ',') );
+        }
+    }
+
+})();
---
+++ js/libs/donut.js
@@ -0,0 +1,151 @@
+(function(){
+	
+    function setDonutConfig( data, duration ){
+        var donutConfig = {
+            plotOptions: {
+                pie: {
+                    center : {
+                        x : 150,
+                        y : 125
+                    },
+                    innerRadius : 50,
+                    outerRadius : 66,
+                    incrementRadius : 16,
+                    originAngle : 45,
+                    animateAngle : 45,
+                    gap : 10,
+                    stroke : {
+                        width : 0
+                    },
+                    shadow : {
+                        enabled : false,
+                        size : 1,
+                        x : 0,
+                        y : 0,
+                        color : "rgba( 0, 0, 0, 0.3 )"
+                    }
+                },
+                label : {
+                    enabled : false
+                }
+            },
+            legend : {
+                enabled : false
+            },
+            animation:{
+                enabled : true,
+                duration: duration || 1000,
+                mode : 'ease'
+            }
+        };
+
+        var series = [], val;
+        for(var i in data){
+            val = data[i].value;
+            series.push({
+                    "name": i,
+                    "data": [
+                        {
+                            "value": val,
+                            "color": data[i].color
+                        },
+                        {
+                            "value": 100 - val,
+                            "color": "#0b2b4c"
+                        }
+                    ]
+                });
+        }
+
+        donutConfig.series = series;
+        return donutConfig;
+    }
+
+    var lines = [];
+
+    function connect( chart, conf ){
+        var label = chart.addElement( '', new kc.Line({
+            x1: conf.x,
+            y1: conf.y,
+            x2: conf.x,
+            y2: conf.y,
+            width: 1,
+            color: conf.color
+        }));
+
+        label.animate({
+        	x2: conf.x + conf.length * conf.dir,
+        }, conf.duration);
+
+        lines.push( label );
+    }
+    
+    window.Donut = {
+    	init : function( id ){
+    		this.container = $('#'+id)[0];
+    		this.chart = this.chart || new kc.PieChart(id);
+    	},
+
+    	update : function( data, duration ){
+    		this.data = data;
+    		this.animDur = duration || 1000;
+
+    		this.conf = setDonutConfig( data, this.animDur );
+    		this.chart.update( this.conf );
+
+    		this.setLabels();
+    	},
+
+    	setLabels : function(){
+
+            var self = this;
+            lines.forEach(function(l){
+                self.chart.canvas.removeShape(l.canvas);
+            });
+
+            $('.donut-label').remove();
+
+    		var list = this.chart.getParamList();
+    		
+    		setTimeout(function(){
+	            list.forEach(function(param, i){
+	                if( i%2 != 0 ) return;
+
+	                var r = param.innerRadius,
+	                	angle = param.startAngle + param.pieAngle - 90 + param.originAngle,
+	                	a = angle/180*Math.PI,
+	                	center = self.conf.plotOptions.pie.center;
+
+	                var x = r * Math.cos(a) + center.x,
+	                    y = r * Math.sin(a) + center.y;
+
+	                var entry = self.data[i/2],
+	                	dir = ( angle < 90 || angle > 270 ) ? 1 : -1,
+	                	length = 80,
+	                	duration = 600;
+
+	                var label = $('<div class="donut-label" style="color:'+entry.color+'">' + entry.label + '</div>').appendTo( self.container );
+	                label.css({
+	                	color : entry.color,
+	                    left : x + 'px',
+	                    top : (y - label.height()/2 ) + 'px',
+	                    webkitTransition : duration + 'ms',
+	                    webkitTransform : 'translate3d('+ ( dir > 0 ? length+5 : -length-label.width()-5 ) +'px, 0, 0)'
+	                })
+
+	                connect( self.chart, {
+	                	x : x,
+	                	y : y,
+	                	dir : dir,
+	                	length : length,
+	                	color : entry.color,
+	                	duration : duration
+	                } );
+
+	            });
+    		}, this.animDur);
+
+    	}
+    }
+
+})();
