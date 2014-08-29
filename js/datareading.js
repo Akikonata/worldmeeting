ac33065e0789b6445193d81fc600e740fe156179
Akikonata <66875692@qq.com>
2014-08-29 13:31:12 +0800

    test

--- js/datareading.js
+++ js/datareading.js
@@ -19,12 +19,13 @@
 	var $onlinecount = $("#onlinecount");
 	var $msgcount = $("#msgcount");
 	chinactx.fillStyle = '#f69701';
-	chinactx.shadowBlur = 10;
+	//chinactx.shadowBlur = 10;
 	chinactx.shadowColor = "#f69701";
 	worldctx.fillStyle = '#f69701';
-	worldctx.shadowBlur = 10;
+	//worldctx.shadowBlur = 10;
 	worldctx.shadowColor = "#f69701";
-
+	chinactx.globalCompositeOperation = "lighter";
+	worldctx.globalCompositeOperation = "lighter";
 	var getData = function() {
 		var Dt = new Date();
 		var hours = Dt.getHours();
@@ -71,43 +72,54 @@
 					contryList[key] = d[key];
 				}
 			}
+			//绘制中国部分
 			var chinaMap = Utils.chinaMap;
-			chinactx.beginPath();
+			chinactx.clearRect(0, 0, 1000, 840);
 			for (var key in chinaMap) {
 				var province = chinaMap[key];
 				var data = provinceList[key];
-				var online_user = data.online_user / 10000;
+				var online_user = parseInt(data.online_user / 30000);
 				var range = province.length;
 				//绘制点
 				for (var i = 0; i < online_user; i++) {
+					chinactx.beginPath();
 					var grid = parseInt(Math.random() * range);
-					var x = Math.random() * 10;
-					var y = Math.random() * 10;
-					chinactx.moveTo(province[grid][0] * 10 + x - 2, province[grid][1] * 10 + y);
-					chinactx.arc(province[grid][0] * 10 + x, province[grid][1] * 10 + y, 2, 0, Math.PI * 2, true);
+					//console.log(online_user);
+					var p = {
+						x: province[grid][0] * 10 + Math.random() * 10,
+						y: province[grid][1] * 10 + Math.random() * 10,
+						size: 1,
+						color: "#f69701"
+					};
+					// var gradient = chinactx.createRadialGradient(p.x, p.y, 0, p.x, p.y, p.size);
+					// gradient.addColorStop(0, "white");
+					// gradient.addColorStop(0.4, "white");
+					// gradient.addColorStop(0.4, p.color);
+					// gradient.addColorStop(1, "black");
+					// chinactx.fillStyle = gradient;
+					chinactx.arc(p.x, p.y, p.size, Math.PI * 2, false);
+					chinactx.fill();
 				}
 			}
-			chinactx.clearRect(0, 0, 1000, 840);
-			chinactx.fill();
-			//世界
+			//绘制世界部分
 			var worldMap = Utils.worldMap;
-			worldctx.beginPath();
+			worldctx.clearRect(0, 0, 1580, 780);
 			for (var key in worldMap) {
 				var contry = worldMap[key];
 				var data = contryList[key];
-				var online_user = data.online_user / 10000;
+				var online_user = data.online_user / 30000;
 				var range = contry.length;
 				//绘制点
 				for (var i = 0; i < online_user; i++) {
+					worldctx.beginPath();
 					var grid = parseInt(Math.random() * range);
 					var x = Math.random() * 10;
 					var y = Math.random() * 10;
 					worldctx.moveTo(contry[grid][0] * 10 + x - 1, contry[grid][1] * 10 + y);
 					worldctx.arc(contry[grid][0] * 10 + x, contry[grid][1] * 10 + y, 1, 0, Math.PI * 2, true);
+					worldctx.fill();
 				}
 			}
-			worldctx.clearRect(0, 0, 1580, 780);
-			worldctx.fill();
 		}
 	}, 1000);
 })();
